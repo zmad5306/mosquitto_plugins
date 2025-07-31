9be5965276b22e8ea5a09377afb485c71fc2da50
Zach Maddox <zmad5306@protonmail.com>
2025-07-31 18:10:51 -0400

    basic auth plugin shell

--- jwt-plugin/jwt_auth.c
+++ jwt-plugin/jwt_auth.c
@@ -1,62 +1,55 @@
-#define MOSQ_PLUGIN_VERSION 5
-
 #include <mosquitto.h>
 #include <mosquitto_broker.h>
 #include <mosquitto_plugin.h>
+#include <stdio.h>
 
-#include <string.h>
-#include <jwt.h>
-
-static char jwt_secret[256] = {0};
-
-static int validate_jwt(const char *token)
+int mosquitto_auth_plugin_version(void)
 {
-    jwt_t *jwt = NULL;
-    if (jwt_decode(&jwt, token, (unsigned char*)jwt_secret, strlen(jwt_secret))) {
-        return MOSQ_ERR_AUTH;
-    }
-    jwt_free(jwt);
+    return MOSQ_AUTH_PLUGIN_VERSION;
+}
+
+int mosquitto_auth_plugin_init(void **user_data, struct mosquitto_opt *opts, int opt_count)
+{
+    *user_data = NULL;
+    mosquitto_log_printf(MOSQ_LOG_INFO, "[JWT-AUTH] Auth plugin initialized");
+    return MOSQ_ERR_SUCCESS;
+}
+
+int mosquitto_auth_plugin_cleanup(void *user_data, struct mosquitto_opt *opts, int opt_count)
+{
+    mosquitto_log_printf(MOSQ_LOG_INFO, "[JWT-AUTH] Auth plugin cleanup");
+    return MOSQ_ERR_SUCCESS;
+}
+
+int mosquitto_auth_security_init(void *user_data, struct mosquitto_opt *opts, int opt_count, bool reload)
+{
+    mosquitto_log_printf(MOSQ_LOG_INFO, "[JWT-AUTH] Security init");
+    return MOSQ_ERR_SUCCESS;
+}
+
+int mosquitto_auth_security_cleanup(void *user_data, struct mosquitto_opt *opts, int opt_count, bool reload)
+{
+    mosquitto_log_printf(MOSQ_LOG_INFO, "[JWT-AUTH] Security cleanup");
     return MOSQ_ERR_SUCCESS;
 }
 
 int mosquitto_auth_unpwd_check(void *user_data, struct mosquitto *client,
                                const char *username, const char *password)
 {
-    if (!password) {
-        mosquitto_log_printf(MOSQ_LOG_ERR, "JWT Auth: No token");
-        return MOSQ_ERR_AUTH;
-    }
-    return validate_jwt(password);
-}
-
-int mosquitto_plugin_version(int supported_version_count, const int *supported_versions)
-{
-    for (int i = 0; i < supported_version_count; i++) {
-        if (supported_versions[i] == 5) {
-            return 5;
-        }
-    }
-    return -1;
-}
-
-int mosquitto_plugin_init(mosquitto_plugin_id_t *identifier, void **user_data,
-                          struct mosquitto_opt *options, int option_count)
-{
-    for (int i = 0; i < option_count; i++) {
-        if (!strcmp(options[i].key, "jwt_secret")) {
-            strncpy(jwt_secret, options[i].value, sizeof(jwt_secret) - 1);
-        }
-    }
-    if (jwt_secret[0] == '\0') {
-        mosquitto_log_printf(MOSQ_LOG_ERR, "JWT Auth: Missing secret");
-        return MOSQ_ERR_INVAL;
-    }
-    mosquitto_log_printf(MOSQ_LOG_INFO, "JWT Auth plugin loaded");
+    mosquitto_log_printf(MOSQ_LOG_INFO, "[JWT-AUTH] Checking username: %s",
+                         username ? username : "(null)");
     return MOSQ_ERR_SUCCESS;
 }
 
-int mosquitto_plugin_cleanup(void *user_data, struct mosquitto_opt *options, int option_count)
+int mosquitto_auth_acl_check(void *user_data, int access, struct mosquitto *client,
+                             const struct mosquitto_acl_msg *msg)
 {
-    mosquitto_log_printf(MOSQ_LOG_INFO, "JWT Auth plugin unloaded");
+    mosquitto_log_printf(MOSQ_LOG_INFO, "[JWT-AUTH] ACL check");
+    return MOSQ_ERR_SUCCESS;
+}
+
+int mosquitto_auth_disconnect(void *user_data, struct mosquitto *client)
+{
+    mosquitto_log_printf(MOSQ_LOG_INFO, "[JWT-AUTH] Client disconnected");
     return MOSQ_ERR_SUCCESS;
 }
